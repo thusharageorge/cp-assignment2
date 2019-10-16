23e07c793cf17abf599a3e4e5b23b106fbaba12e
thusharageorge <56623067+thusharageorge@users.noreply.github.com>
2019-10-16 11:46:13 +0530

    Create Q_2.c

---
+++ Q_2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+  int i,j;
+  int space=0;
+  for(i=1;i<=5;i++)
+  {
+  	for(j=5;j>=space;j--)
+	{
+		printf(" ");
+	}
+	for(j=1;j<=i;j++)
+	{
+		printf("%d",j);
+	}
+	for(j=i-1;j>=1;j--)
+	{
+		printf("%d",j);
+	}
+	printf("\n");
+	space++;
+  }
+}
+
+    
